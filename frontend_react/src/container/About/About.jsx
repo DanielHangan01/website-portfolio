b656865752ffdd940acfcc13d110a2eaa4796d63
DanielHangan01 <danhangan@hotmail.com>
2023-12-05 12:40:05 +0100

    Linked sanity backend with react frontend in abouts

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -2,16 +2,15 @@ import React, { useState, useEffect } from 'react'
 
 import './About.scss'
 import { motion } from 'framer-motion'
-
+import { urlFor, client } from '../../client'
 import { images } from '../../constants'
 
-const abouts = [
-  { title: 'Full Stack Developer', description: 'I am a good web developer.', imgUrl: images.about01 },
-  { title: 'Data Analyst', description: 'My strongest point is mathematics and probability theory in particular.', imgUrl: images.about02}
-
-]
-
 const About = () => {
+  const [abouts, setAbouts] = useState([])
+  useEffect(() => {
+    const query = '*[_type == "abouts"]'
+    client.fetch(query).then((data) => setAbouts(data))
+  }, [])
   return (
     <>
       <h2 className="head-text"><span>Good Design</span> <br />means <br /> <span>Good Business</span></h2>
@@ -19,13 +18,13 @@ const About = () => {
       <div className="app__profiles">
         {abouts.map((about, index) => (
           <motion.div
-            whileInView={{ opacity: 1 }}
+            whileInView={{ opacity: 1.0 }}
             whileHover={{ scale: 1.1 }}
             transition={{ duration: 0.5, type: 'tween' }}
             className="app__profile-item"
             key={about.title + index}
           >
-            <img src={about.imgUrl} alt={about.title} />
+            <img src={urlFor(about.imgUrl)} alt={about.title} />
             <h2 className="bold-text" style={{ marginTop: 20 }}>{about.title}</h2>
             <p className="p-text" style={{ marginTop: 10 }}>{about.description}</p>
           </motion.div>
